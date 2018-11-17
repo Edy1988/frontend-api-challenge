3629970a438b646ce6bcbfa2f2ca5e3628d89878
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-11-17 20:43:04 +0000

    Set up and frst test

---
+++ spec/signUpSpec.js
@@ -0,0 +1,10 @@
+const { User } = require('../lib/user')
+
+ describe("SignUp", function(){
+  it("creates a user with a name, username and email", function() {
+    var user = new User("edyta", "edy", "edy@email.com")
+    expect(user.name).toEqual("edyta")
+    expect(user.username).toEqual("edy")
+    expect(user.email).toEqual("edy@email.com")
+  });
+});
