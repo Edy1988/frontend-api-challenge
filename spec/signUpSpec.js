b59e4773355de5bfbe9e590f517a67d243262ed7
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-11-17 21:20:07 +0000

    Added password to user.

--- spec/signUpSpec.js
+++ spec/signUpSpec.js
@@ -1,10 +1,12 @@
 const { User } = require('../lib/user')
 
- describe("SignUp", function(){
+describe("SignUp", function(){
   it("creates a user with a name, username and email", function() {
-    var user = new User("edyta", "edy", "edy@email.com")
+    var user = new User("edyta", "edy", "edy@email.com", "password")
     expect(user.name).toEqual("edyta")
     expect(user.username).toEqual("edy")
     expect(user.email).toEqual("edy@email.com")
+    expect(user.password).toEqual("password")
+
   });
 });
