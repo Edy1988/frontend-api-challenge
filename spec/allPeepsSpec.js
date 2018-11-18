aaf2abd4e4516c025ef3f99fe26e8bb0a803c525
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-11-18 20:42:33 +0000

    Changed test and added username.

--- spec/allPeepsSpec.js
+++ spec/allPeepsSpec.js
@@ -1,8 +1,9 @@
 const { Peep } = require('../lib/peep')
 
 describe("Peep", function(){
-  it("has a text", function(){
-    var peep = new Peep("this is a peep");
+  it("has a text and username", function(){
+    var peep = new Peep("edy","this is a peep");
+    expect(peep.username).toEqual("edy");
     expect(peep.text).toEqual("this is a peep");
   })
 })
